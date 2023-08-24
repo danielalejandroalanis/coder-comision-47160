80ad354c2e41a66304748ecf7a1414feb3ba04c3
Profe Dani <74632823+danielalejandroalanis@users.noreply.github.com>
2023-08-23 21:44:52 -0300

    Se agrega clase 6 de asincronia y useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,112 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 
 //Styles
 import MainLayout from "./layout/MainLayout";
+import PersonaComponent from "./components/PersonaComponent/PersonaComponent";
+
+//Promesas
+const flag = false;
+const promesa = new Promise((resolve, reject) => {
+  if (flag) {
+    resolve("Tarea resuelta");
+  } else {
+    reject("Tarea rechazada");
+  }
+});
 
 const App = () => {
-  const [state, setState]= useState(false);
-  const [stateDos, setStateDos]= useState(false);
+  const [state, setState] = useState(false);
+  const [stateDos, setStateDos] = useState(false);
+  const [arrObj, setArrObj] = useState([
+    {
+      id: 1,
+      nombre: "Juan",
+      apellido: "Perez",
+      edad: 30,
+    },
+    {
+      id: 2,
+      nombre: "Pepito",
+      apellido: "Perez",
+      edad: 30,
+    },
+    {
+      id: 3,
+      nombre: "Maria",
+      apellido: "Perez",
+      edad: 30,
+    },
+  ]);
   const inputName = useRef(null);
   const inputAge = useRef(null);
   const itemList = useRef(null);
 
-  useEffect(() => {
+  // const obj = {
+  //   nombre: "Juan",
+  //   apellido: "Perez",
+  //   edad: 30,
+  // }
 
+  // console.table(obj)
+  // console.debug("debug");
+
+  //MAP
+  // const arrObj = [
+  //   {
+  //     id: 1,
+  //     nombre: "Juan",
+  //     apellido: "Perez",
+  //     edad: 30,
+  //   },
+  //   {
+  //     id: 2,
+  //     nombre: "Pepito",
+  //     apellido: "Perez",
+  //     edad: 30,
+  //   },
+  //   {
+  //     id: 3,
+  //     nombre: "Maria",
+  //     apellido: "Perez",
+  //     edad: 30,
+  //   },
+  // ];
+
+  // console.log(arrObj.filter(item => item.nombre === "Maria"))
+
+  arrObj.map((itemPersona) => {
+    console.log(itemPersona.nombre);
+  });
+
+  useEffect(() => {
+    promesa
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        throw new Error(err);
+      })
+      .finally(() => {
+        console.log("Se ejecuta siempre");
+      });
+  }, []);
+
+  useEffect(() => {
     console.log("useeffect con dependencias vacias");
     // inputName.current.focus();
-  }, [])
+  }, []);
 
   useEffect(() => {
-
     console.log("useeffect con el estado como dependencia");
-    
-    return () => { 
-      console.log("componente desmontado");
-    }
 
-  }, [state, stateDos])
+    return () => {
+      console.log("componente desmontado");
+    };
+  }, [state, stateDos]);
 
   useEffect(() => {
-
     console.log("useeffect sin dependencias");
     // inputName.current.focus();
-  })
-
+  });
 
   // const handleOnSubmit = (e) => {
   //   e.preventDefault();
@@ -51,9 +127,24 @@ const App = () => {
       {state}
       <button onClick={() => setState(!state)}>Click me!</button>
       <button onClick={() => setStateDos(!stateDos)}>Saludo</button>
+      <div>
+        {
+          // arrObj.map((itemPersona, index) => {
+          //   return <PersonaComponent key={index} itemPersona={itemPersona}/>
+          // })
+          arrObj.map((itemPersona) => {
+            return (
+              <PersonaComponent
+                key={itemPersona.id}
+                itemPersona={itemPersona}
+              />
+            );
+          })
+        }
+      </div>
     </div>
 
-    // <MainLayout> 
+    // <MainLayout>
     //   <ItemListContainer greeting="Bienvenidos a mi tienda..."/>
     //   <form onSubmit={handleOnSubmit}>
     //     <input ref={inputName} type="text" placeholder="Ingrese su nombre" />
