5488ebdb58adb96e7833aa9975dcb9c1fa9682cd
Profe Dani <74632823+danielalejandroalanis@users.noreply.github.com>
2023-08-28 20:29:51 -0300

    se agrega clase 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,157 +1,52 @@
-//Components
-import { useEffect, useRef, useState } from "react";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-
-//Styles
+import { useEffect, useState } from "react";
 import MainLayout from "./layout/MainLayout";
-import PersonaComponent from "./components/PersonaComponent/PersonaComponent";
-
-//Promesas
-const flag = false;
-const promesa = new Promise((resolve, reject) => {
-  if (flag) {
-    resolve("Tarea resuelta");
-  } else {
-    reject("Tarea rechazada");
-  }
-});
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+// Importando axios
+import axios from "axios";
 
 const App = () => {
-  const [state, setState] = useState(false);
-  const [stateDos, setStateDos] = useState(false);
-  const [arrObj, setArrObj] = useState([
-    {
-      id: 1,
-      nombre: "Juan",
-      apellido: "Perez",
-      edad: 30,
-    },
-    {
-      id: 2,
-      nombre: "Pepito",
-      apellido: "Perez",
-      edad: 30,
-    },
-    {
-      id: 3,
-      nombre: "Maria",
-      apellido: "Perez",
-      edad: 30,
-    },
-  ]);
-  const inputName = useRef(null);
-  const inputAge = useRef(null);
-  const itemList = useRef(null);
+  const [titleData, setTitleData] = useState("");
+  //POST - Agrega un producto (url, body, headers)
 
-  // const obj = {
-  //   nombre: "Juan",
-  //   apellido: "Perez",
-  //   edad: 30,
-  // }
+  // useEffect(() => {
+  //   axios
+  //     .post(
+  //       "https://dummyjson.com/products/add",
+  //       {
+  //         title: titleData,
+  //       },
+  //       {
+  //         "Content-Type": "application/json",
+  //       }
+  //     )
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err));
+  // }, []);
 
-  // console.table(obj)
-  // console.debug("debug");
-
-  //MAP
-  // const arrObj = [
-  //   {
-  //     id: 1,
-  //     nombre: "Juan",
-  //     apellido: "Perez",
-  //     edad: 30,
-  //   },
-  //   {
-  //     id: 2,
-  //     nombre: "Pepito",
-  //     apellido: "Perez",
-  //     edad: 30,
-  //   },
-  //   {
-  //     id: 3,
-  //     nombre: "Maria",
-  //     apellido: "Perez",
-  //     edad: 30,
-  //   },
-  // ];
-
-  // console.log(arrObj.filter(item => item.nombre === "Maria"))
-
-  arrObj.map((itemPersona) => {
-    console.log(itemPersona.nombre);
-  });
-
-  useEffect(() => {
-    promesa
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        throw new Error(err);
-      })
-      .finally(() => {
-        console.log("Se ejecuta siempre");
-      });
-  }, []);
-
-  useEffect(() => {
-    console.log("useeffect con dependencias vacias");
-    // inputName.current.focus();
-  }, []);
-
-  useEffect(() => {
-    console.log("useeffect con el estado como dependencia");
-
-    return () => {
-      console.log("componente desmontado");
-    };
-  }, [state, stateDos]);
-
-  useEffect(() => {
-    console.log("useeffect sin dependencias");
-    // inputName.current.focus();
-  });
-
-  // const handleOnSubmit = (e) => {
-  //   e.preventDefault();
-  //   inputName.current.focus();
-  //   console.log(inputName.current.value);
-  //   console.log(inputAge.current.onFocus);
-  //   console.log(inputName)
-  //   console.log("Formulario enviado");
-  //   console.log(itemList)
-  // };
+  const handleNewProduct = () => {
+    console.log(titleData)
+    axios
+      .post(
+        "https://dummyjson.com/products/add",
+        {
+          title: titleData,
+        },
+        {
+          "Content-Type": "application/json",
+        }
+      )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
 
   return (
-    //HOC High Order Components - es un patron de diseño
-    <div>
-      {state}
-      <button onClick={() => setState(!state)}>Click me!</button>
-      <button onClick={() => setStateDos(!stateDos)}>Saludo</button>
-      <div>
-        {
-          // arrObj.map((itemPersona, index) => {
-          //   return <PersonaComponent key={index} itemPersona={itemPersona}/>
-          // })
-          arrObj.map((itemPersona) => {
-            return (
-              <PersonaComponent
-                key={itemPersona.id}
-                itemPersona={itemPersona}
-              />
-            );
-          })
-        }
-      </div>
-    </div>
-
-    // <MainLayout>
-    //   <ItemListContainer greeting="Bienvenidos a mi tienda..."/>
-    //   <form onSubmit={handleOnSubmit}>
-    //     <input ref={inputName} type="text" placeholder="Ingrese su nombre" />
-    //     <input ref={inputAge} type="number" placeholder="Ingrese su edad"/>
-    //     <input type="submit" value="Enviar" />
-    //   </form>
-    // </MainLayout>
+    <MainLayout>
+      <ItemListContainer greeting="Bienvenidos a mi tienda..." />
+      <form>
+        <input type="text" value={titleData} onChange={(e) => setTitleData(e.target.value)}/>
+        <button onClick={handleNewProduct}>Crear producto</button>
+      </form>
+    </MainLayout>
   );
 };
 
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,33 @@
 // import "./ItemListContainer.css";
+import { useEffect, useState } from "react";
 import styles from "./ItemListContainer.module.css";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const ItemListContainer = ({ greeting }) => {
-  return <div className={styles.greetingStyles}>{greeting}</div>;
+  // const [products, setProducts] = useState([]);
+  // useEffect(() => {
+  //   axios
+  //     .get("https://dummyjson.com/products")
+  //     .then((res) => setProducts(res.data.products))
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  // return products.map((product) => {
+  //   return (
+  //     <Card style={{ width: "18rem" }} key={product.id}>
+  //       <Card.Img variant="top" src={product.thumbnail} />
+  //       <Card.Body>
+  //         <Card.Title>{product.title}</Card.Title>
+  //         <Card.Text>
+  //           {product.description}
+  //         </Card.Text>
+  //       </Card.Body>
+  //     </Card>
+  //   );
+  // });
+  return <div>{greeting}</div>
 };
 
 export default ItemListContainer;
